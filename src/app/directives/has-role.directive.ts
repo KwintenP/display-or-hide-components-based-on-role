43487c66c7fcf9b6d0d3b58fea6cb13fde690528
Kwinten Pisman <kwinten.pisman@gmail.com>
2018-01-16 19:03:03 +0100

    Updated documentation.

--- src/app/directives/has-role.directive.ts
+++ src/app/directives/has-role.directive.ts
@@ -1,12 +1,20 @@
-import {Directive, Input, OnInit, TemplateRef, ViewContainerRef} from '@angular/core';
-import {Subject} from 'rxjs/Subject';
-import {RolesService} from '../services/roles.service';
-import {takeUntil} from 'rxjs/operators';
+import {
+  Directive,
+  Input,
+  OnDestroy,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef
+} from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { RolesService } from '../services/roles.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Directive({
   selector: '[appHasRole]'
 })
-export class HasRoleDirective implements OnInit {
+export class HasRoleDirective implements OnInit, OnDestroy {
+  // the role the user must have
   @Input() appHasRole: string;
 
   stop$ = new Subject();
@@ -14,9 +22,12 @@ export class HasRoleDirective implements OnInit {
   isVisible = false;
 
   /**
-   * @param {ViewContainerRef} viewContainerRef -- the location where we need to render the templateRef
-   * @param {TemplateRef<any>} templateRef -- the templateRef that needs to be rendered if the user has the role
-   * @param {RolesService} rolesService -- will give us access to the roles a user has
+   * @param {ViewContainerRef} viewContainerRef
+   * 	-- the location where we need to render the templateRef
+   * @param {TemplateRef<any>} templateRef
+   *   -- the templateRef to be potentially rendered
+   * @param {RolesService} rolesService
+   *   -- will give us access to the roles a user has
    */
   constructor(
     private viewContainerRef: ViewContainerRef,
@@ -25,27 +36,36 @@ export class HasRoleDirective implements OnInit {
   ) {}
 
   ngOnInit() {
-    //  We subscribe to the roles to know the roles the user has
+    //  We subscribe to the roles$ to know the roles the user has
     this.rolesService.roles$.pipe(takeUntil(this.stop$)).subscribe(roles => {
-      // If he doesn't hold any roles, we clear the viewContainerRef
+      // If he doesn't have any roles, we clear the viewContainerRef
       if (!roles) {
         this.viewContainerRef.clear();
       }
-      // If the user has the role needed to render this component we can add it
+      // If the user has the role needed to
+      // render this component we can add it
       if (roles.includes(this.appHasRole)) {
-        // If it is already visible (which can happen if his roles changed) we do not need to add it a second time
+        // If it is already visible (which can happen if
+        // his roles changed) we do not need to add it a second time
         if (!this.isVisible) {
-          // We update the `isVisible` property and add it to the view using the 'createEmbeddedView' method of the
-          // viewContainerRef
+          // We update the `isVisible` property and add the
+          // templateRef to the view using the
+          // 'createEmbeddedView' method of the viewContainerRef
           this.isVisible = true;
           this.viewContainerRef.createEmbeddedView(this.templateRef);
         }
       } else {
-        // If the user does not have the role, we update the `isVisible` property and clear the contents of the
-        // viewContainerRef
+        // If the user does not have the role,
+        // we update the `isVisible` property and clear
+        // the contents of the viewContainerRef
         this.isVisible = false;
         this.viewContainerRef.clear();
       }
     });
+  }
+
+  // Clear the subscription on destroy
+  ngOnDestroy() {
+    this.stop$.next();
   }
 }
